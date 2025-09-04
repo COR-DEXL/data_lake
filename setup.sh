29f1e6253b49e673444cdc6cec6cd3f840912ccd
Augusto J M Fonseca <ajosemf@gmail.com>
2025-09-04 10:47:49 -0300

    build(minio): update container configuration
    - Pin MinIO image version to ensure reproducibility
    - Change exposed ports (9100/9101) to avoid conflict with Jupyter

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ mkdir -p $PWD/datalake/curated
 
 docker run \
   --name minio-cor \
-  -p 9000:9000 \
-  -p 9001:9001 \
+  -p 9100:9000 \
+  -p 9101:9001 \
   -d \
   --user $(id -u):$(id -g) \
   --env-file config/.env \
   -v $PWD/datalake:/data \
-  minio/minio server /data --console-address ":9001"
+  minio/minio:RELEASE.2025-04-08T15-41-24Z server /data --console-address ":9001"
